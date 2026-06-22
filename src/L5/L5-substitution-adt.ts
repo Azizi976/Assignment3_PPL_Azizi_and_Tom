05b54a3d758b1f37a73aee6e86aae94ea748d6c6
Azizi976 <Tal.azizi123@gmail.com>
2026-06-22 16:09:23 +0300

    3.3.b complete

--- src/L5/L5-substitution-adt.ts
+++ src/L5/L5-substitution-adt.ts
@@ -10,7 +10,7 @@ import { format } from '../shared/format';
 // lists of variables and type expression.
 // The empty substitution is [[], []]
 
-export type Sub = {tag: "Sub"; vars: TVar[]; tes: TExp[]; }
+export type Sub = { tag: "Sub"; vars: TVar[]; tes: TExp[]; }
 export const isSub = (x: any): x is Sub => x.tag === "Sub";
 
 // Constructors:
@@ -29,19 +29,20 @@ export const isSub = (x: any): x is Sub => x.tag === "Sub";
 export const makeSub = (vars: TVar[], tes: TExp[]): Result<Sub> =>
     mapv(zipWithResult(checkNoOccurrence, vars, tes), _ => ({ tag: "Sub", vars: vars, tes: tes }));
 
-export const makeEmptySub = (): Sub => ({tag: "Sub", vars: [], tes: []});
+export const makeEmptySub = (): Sub => ({ tag: "Sub", vars: [], tes: [] });
 
 // Purpose: when attempting to bind tvar to te in a sub - check whether tvar occurs in te.
 // Return error if a circular reference is found.
 export const checkNoOccurrence = (tvar: TVar, te: TExp): Result<true> => {
     const check = (e: TExp): Result<true> =>
-        isTVar(e) ? ((e.var === tvar.var) ? bind(unparseTExp(te), up => makeFailure(`Occur check error - circular sub ${tvar.var} in ${format(up)}`)) : 
-                                            makeOk(true)) :
-        isAtomicTExp(e) ? makeOk(true) :
-        isProcTExp(e) ? bind(mapResult(check, e.paramTEs), _ => check(e.returnTE)) :
-        isListTExp(e) ?
-            makeFailure("HW3 3.2.a - Implement this branch") :
-        makeFailure(`Bad type expression ${e} in ${format(te)}`);
+        isTVar(e) ? ((e.var === tvar.var) ? bind(unparseTExp(te), up => makeFailure(`Occur check error - circular sub ${tvar.var} in ${format(up)}`)) :
+            makeOk(true)) :
+            isAtomicTExp(e) ? makeOk(true) :
+                isProcTExp(e) ? bind(mapResult(check, e.paramTEs), _ => check(e.returnTE)) :
+                    isListTExp(e) ?
+                        // 3.2.a - checks the item of list if the exp is list
+                        check(e.itemTE) :
+                        makeFailure(`Bad type expression ${e} in ${format(te)}`);
     return check(te);
 };
 
@@ -50,10 +51,10 @@ export const isEmptySub = (sub: any): boolean => isSub(sub) && isEmpty(sub.vars)
 // Purpose: If v is in sub.vars - return corresponding te, else v unchanged.
 export const subGet = (sub: Sub, v: TVar): TExp => {
     const lookup = (vars: TVar[], tes: TExp[]): TExp =>
-        isNonEmptyList<TVar>(vars) && isNonEmptyList<TExp>(tes) ? 
+        isNonEmptyList<TVar>(vars) && isNonEmptyList<TExp>(tes) ?
             eqTVar(first(vars), v) ? first(tes) :
-            lookup(rest(vars), rest(tes)) :
-        v;
+                lookup(rest(vars), rest(tes)) :
+            v;
 
     return lookup(sub.vars, sub.tes);
 };
@@ -66,25 +67,26 @@ export const subGet = (sub: Sub, v: TVar): TExp => {
 // "(number * boolean -> number)"
 export const applySub = (sub: Sub, te: TExp): TExp =>
     isEmptySub(sub) ? te :
-    isAtomicTExp(te) ? te :
-    isTVar(te) ? subGet(sub, te) :
-    isProcTExp(te) ? makeProcTExp(map((te) => applySub(sub, te), te.paramTEs), applySub(sub, te.returnTE)) :
-    /* isListTExp(te) ? // HW3 3.2.b - Implement this branch  : */
-    te;
+        isAtomicTExp(te) ? te :
+            isTVar(te) ? subGet(sub, te) :
+                isProcTExp(te) ? makeProcTExp(map((te) => applySub(sub, te), te.paramTEs), applySub(sub, te.returnTE)) :
+                    // 3.2.b - recursive call to substitue the items.
+                    isListTExp(te) ? makeListTExp(applySub(sub, te.itemTE)) :
+                        te;
 
 // ============================================================
 // Purpose: Returns the composition of substitutions s.t.:
 //  applySub(result, te) === applySub(sub2, applySub(sub1, te))
 export const combineSub = (sub1: Sub, sub2: Sub): Result<Sub> =>
     isEmptySub(sub1) ? makeOk(sub2) :
-    isEmptySub(sub2) ? makeOk(sub1) :
-    combine(sub1, sub2.vars, sub2.tes);
+        isEmptySub(sub2) ? makeOk(sub1) :
+            combine(sub1, sub2.vars, sub2.tes);
 
 const combine = (sub: Sub, vars: TVar[], tes: TExp[]): Result<Sub> =>
-    isNonEmptyList<TVar>(vars) && isNonEmptyList<TExp>(tes) ? 
-        bind(extendSub(sub, first(vars), first(tes)), 
-             (extSub: Sub) => combine(extSub, rest(vars), rest(tes))) :
-    makeOk(sub);
+    isNonEmptyList<TVar>(vars) && isNonEmptyList<TExp>(tes) ?
+        bind(extendSub(sub, first(vars), first(tes)),
+            (extSub: Sub) => combine(extSub, rest(vars), rest(tes))) :
+        makeOk(sub);
 
 
 // Purpose: extend a substitution with one pair (tv, te)
@@ -93,5 +95,5 @@ export const extendSub = (sub: Sub, v: TVar, te: TExp): Result<Sub> =>
     bind(makeSub([v], [te]), (sub2: Sub) => {
         const updatedTEs = map((te) => applySub(sub2, te), sub.tes);
         return includes(v.var, map(prop('var'), sub.vars)) ? makeSub(sub.vars, updatedTEs) :
-               makeSub(cons(v, sub.vars), cons(te, updatedTEs));
+            makeSub(cons(v, sub.vars), cons(te, updatedTEs));
     });
